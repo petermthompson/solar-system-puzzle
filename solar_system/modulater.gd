8446633be7e7fd8f808f7fc9f9815ec574069d9a
petermthompson <peter.m.thompson@protonmail.com>
2024-09-22 22:10:09 -0700

    Attempt to fix lighting (part 2)

--- solar_system/modulater.gd
+++ solar_system/modulater.gd
@@ -3,7 +3,9 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if JavaScriptBridge.eval("navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)"):
+	if OS.has_feature("web_android") or OS.has_feature("web_ios"):
 		modulate = Color.WHITE
+		JavaScriptBridge.eval("console.log(\'low-end gpu\')")
 	else:
 		modulate = Color.WEB_GRAY
+		JavaScriptBridge.eval("console.log(\'high-end gpu\')")
